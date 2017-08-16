93e6b790ba8d73ba80ac554c5f0010d9afdebbf4
Rajanikant Tenguria <rajanikant.tenguria@vit.edu.in>
2017-08-16 14:15:34 +0530

    Update kangaroo.py

--- Algorithms/Implementation/kangaroo.py
+++ Algorithms/Implementation/kangaroo.py
@@ -1,11 +1,16 @@
+"""
 # Author : Rajanikant Tenguria
 
-#!/bin/python
+Problem 003
+There are two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity). 
+The first kangaroo starts at location and moves at a rate of meters per jump. The second kangaroo starts at location and 
+moves at a rate of meters per jump. Given the starting locations and movement rates for each kangaroo, can you determine 
+if they'll ever land at the same location at the same time?
 
-import sys
+"""
 
-x1, v1, x2, v2 = raw_input().strip().split(' ')
-x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
+x1, v1, x2, v2 = raw_input().strip().split(' ')                                          
+x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]                                # take input of positions and their velocities
 if(x1<x2 and v1<v2):
     print "NO"
 else:
