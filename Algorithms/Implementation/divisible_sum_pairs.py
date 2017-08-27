e13573ddabbfde0843008f81b050aee3f5fd171a
Rajanikant Tenguria <rajanikant.tenguria@vit.edu.in>
2017-08-27 11:22:05 +0530

    Update divisible_sum_pairs.py

--- Algorithms/Implementation/divisible_sum_pairs.py
+++ Algorithms/Implementation/divisible_sum_pairs.py
@@ -1,14 +1,18 @@
-# Author : Rajanikant Tenguria
-
-#!/bin/python
+"""
+Author    : Rajanikant Tenguria        
+Language  : Python 2
+Problem   : https://www.hackerrank.com/challenges/between-two-sets
+Avg Time  : 0s
+% Success : 88.15 (32162/36484)
+"""
 
 import sys
-cnt=0
-n, k = raw_input().strip().split(' ')
+cnt = 0 
+n, k = raw_input().strip().split(' ')                             # take no of elements in array and number it should be divisible by.
 n, k = [int(n), int(k)]
 a = map(int, raw_input().strip().split(' '))
 for i in range(n):
     for j in range(i+1,n):
-        if (a[i]+a[j])%k==0:
-            cnt+=1
+        if (a[i] + a[j]) % k == 0:                                # if remainder == 0, it is divisible by k
+            cnt +=     1                                          # increase counter 
 print cnt                
